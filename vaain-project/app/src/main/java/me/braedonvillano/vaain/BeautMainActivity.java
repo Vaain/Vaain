35aa3e0bb88251cab30546857c4cf95773c9bf69
jtopete135 <jtopete@stanford.edu>
2018-07-19 16:32:34 -0700

    beaut profile with tabs, but empty fragments

--- vaain-project/app/src/main/java/me/braedonvillano/vaain/BeautMainActivity.java
+++ vaain-project/app/src/main/java/me/braedonvillano/vaain/BeautMainActivity.java
@@ -1,7 +1,13 @@
 package me.braedonvillano.vaain;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class BeautMainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class BeautMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beaut_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment beautProfileFragment = new BeautProfileFragment();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_profile:
+                                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                                fragmentTransaction.replace(R.id.beaut_frag_holder, beautProfileFragment).commit();
+                                return true;
+                            case R.id.action_requests:
+//                                fragmentTransaction = fragmentManager.beginTransaction();
+//                                fragmentTransaction.replace(R.id.frag_placeholder,searchFragment).commit();
+                                return true;
+                        }
+                        return false;
+                    }
+
+                });
     }
 }
