2455076548019a113cc32ef53cf3e7bf190aa450
Braedon Villano <braedonvillano@fb.com>
2018-07-18 17:28:36 -0700

    added the beautician main activity, and handled login

---
+++ vaain-project/app/src/main/java/me/braedonvillano/vaain/BeautMainActivity.java
@@ -0,0 +1,13 @@
+package me.braedonvillano.vaain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeautMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beaut_main);
+    }
+}
