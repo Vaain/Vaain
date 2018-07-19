fe44f540067f895b3a76949a6e2913f3d9758f6a
jtopete135 <35283607+jtopete135@users.noreply.github.com>
2018-07-19 14:57:32 -0700

    Profile fragment with tabs (#30)
    * profile pic and info for profile implemented
    
    * implemented some aspects of the tabs and fragments in profile page
    
    * Client profile page with tabs
    
    * profile fragment with tabs and better design

--- vaain-project/app/src/main/java/me/braedonvillano/vaain/MainActivity.java
+++ vaain-project/app/src/main/java/me/braedonvillano/vaain/MainActivity.java
@@ -1,12 +1,12 @@
 package me.braedonvillano.vaain;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
