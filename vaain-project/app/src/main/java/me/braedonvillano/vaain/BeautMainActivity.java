353f5fa57f80f25ad1f99fb6425045f3c0d39a3a
Braedon Villano <braedonvillano@fb.com>
2018-07-19 15:11:51 -0700

    minor changes

--- vaain-project/app/src/main/java/me/braedonvillano/vaain/BeautMainActivity.java
+++ vaain-project/app/src/main/java/me/braedonvillano/vaain/BeautMainActivity.java
@@ -1,13 +1,48 @@
 package me.braedonvillano.vaain;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class BeautMainActivity extends AppCompatActivity {
+
+    // define your fragments here
+    final RequestsFragment requestsFragment = new RequestsFragment();
+    final BusinessFragment businessFragment = new BusinessFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beaut_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.beaut_bottom_navigation);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.beauts_frag_placeholder, requestsFragment).commit();
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    switch (item.getItemId()) {
+                        case R.id.action_requests:
+                            fragmentTransaction.replace(R.id.beauts_frag_placeholder, requestsFragment).commit();
+                            return true;
+                        case R.id.action_business:
+                            fragmentTransaction.replace(R.id.beauts_frag_placeholder, businessFragment).commit();
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+
+            });
     }
 }
---
+++ vaain-project/app/src/main/java/me/braedonvillano/vaain/BusinessFragment.java
@@ -0,0 +1,78 @@
+package me.braedonvillano.vaain;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BusinessFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public interface BusinessFragmentInterface {
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private BusinessFragmentInterface businessInterface;
+
+    public BusinessFragment() {
+    }
+
+    public static BusinessFragment newInstance(String param1, String param2) {
+        BusinessFragment fragment = new BusinessFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_business, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (businessInterface != null) {
+            businessInterface.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+//        if (context instanceof BusinessFragmentInterface) {
+//            businessInterface = (BusinessFragmentInterface) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        businessInterface = null;
+    }
+}
